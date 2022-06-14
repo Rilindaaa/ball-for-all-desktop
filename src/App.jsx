731fb429e4950fb8facffbfc05a3cfd59095f4ac
rilindaxhaqkaj <rilinda@centralsoft.io>
2022-06-15 00:04:47 +0200

    feat: fetchClubs/Players and deleteClubs/Players

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 import "./theme/App.scss";
 import { AuthContextProvider } from "./contexts/AuthContext";
-import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import Router from "./routes";
 import MainLayout from "./layouts/MainLayout";
 import theme from "./theme/MuiTheme";
+import { ConfirmProvider } from "material-ui-confirm";
 
 function App() {
   return (
     <AuthContextProvider>
-      <ThemeProvider theme={theme}>
-        <div className="App">
-          <MainLayout>
-            <Router />
-          </MainLayout>
-        </div>
-      </ThemeProvider>
+      <ConfirmProvider>
+        <ThemeProvider theme={theme}>
+          <div className="App">
+            <MainLayout>
+              <Router />
+            </MainLayout>
+          </div>
+        </ThemeProvider>
+      </ConfirmProvider>
     </AuthContextProvider>
   );
 }
--- src/components/PlayersTable/PlayersTable.jsx
+++ src/components/PlayersTable/PlayersTable.jsx
@@ -7,20 +7,33 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import styles from "./PlayersTable.module.scss";
-
-function createData(name, calories, fat, carbs, protein) {
-  return { name, calories, fat, carbs, protein };
-}
-
-const rows = [
-  createData(1, "Rilinda Xhaqkaj", "15/03/2001", "rx47891@ubt-uni.net"),
-  createData(2, "Rilinda Xhaqkaj", "15/03/2001", "rx47891@ubt-uni.net"),
-  createData(3, "Rilinda Xhaqkaj", "15/03/2001", "rx47891@ubt-uni.net"),
-  createData(4, "Rilinda Xhaqkaj", "15/03/2001", "rx47891@ubt-uni.net"),
-  createData(5, "Rilinda Xhaqkaj", "15/03/2001", "rx47891@ubt-uni.net"),
-];
+import { getAllPlayers, deletePlayer } from "../../api/ApiMethods";
+import { useEffect, useState } from "react";
+import { useConfirm } from "material-ui-confirm";
 
 export default function PlayersTable() {
+  const [players, setPlayers] = useState([]);
+  const confirm = useConfirm();
+
+  const fetchPlayers = async () => {
+    const players = await getAllPlayers();
+    setPlayers(players);
+    console.log("players", players);
+  };
+
+  const handleDeletePlayer = async (playerId) => {
+    confirm({
+      description: "Are you sure you want to delete this player?",
+    }).then(async () => {
+      await deletePlayer(playerId);
+      await fetchPlayers();
+    });
+  };
+
+  useEffect(() => {
+    fetchPlayers();
+  }, []);
+
   return (
     <TableContainer
       style={{
@@ -38,22 +51,39 @@ export default function PlayersTable() {
         <TableHead>
           <TableRow className={styles.tableHeadRow}>
             <TableCell>NR</TableCell>
-            <TableCell>FULLNAME</TableCell>
-            <TableCell>DATE OF BIRTH</TableCell>
+            <TableCell>FIRSTNAME</TableCell>
+            <TableCell>LASTNAME</TableCell>
+            <TableCell>BIRTHDATE</TableCell>
             <TableCell>EMAIL</TableCell>
+            <TableCell>CITY</TableCell>
+            <TableCell>FOOT</TableCell>
+            <TableCell>POSITION</TableCell>
+            <TableCell>WEIGHT</TableCell>
+            <TableCell>HEIGHT</TableCell>
             <TableCell>ACTION</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.name}>
-              <TableCell component="th" scope="row">
-                {row.name}
+          {players?.map((player, i) => (
+            <TableRow key={player.UserId}>
+              <TableCell>{++i}</TableCell>
+              <TableCell scope="row">{player.Player?.firstName}</TableCell>
+              <TableCell>{player.Player?.lastName}</TableCell>
+              <TableCell>{player.birthDate}</TableCell>
+              <TableCell>{player.email}</TableCell>
+              <TableCell>{player.Player?.city}</TableCell>
+              <TableCell>{player.Player?.foot}</TableCell>
+              <TableCell>{player.Player?.position}</TableCell>
+              <TableCell>{player.Player?.weight}</TableCell>
+              <TableCell>{player.Player?.height}</TableCell>
+              <TableCell>
+                <span
+                  style={{ cursor: "pointer" }}
+                  onClick={() => handleDeletePlayer(player.id)}
+                >
+                  Delete
+                </span>
               </TableCell>
-              <TableCell>{row.calories}</TableCell>
-              <TableCell>{row.fat}</TableCell>
-              <TableCell>{row.carbs}</TableCell>
-              <TableCell>Delete</TableCell>
             </TableRow>
           ))}
         </TableBody>
